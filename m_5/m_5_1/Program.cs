0fef526a4f9fe7b7a024ea51d4f956344a9a8cbb
刘家熊 <1026167476@qq.com>
2020-10-20 11:37:48 +0800

    5/crawler

--- m_5/m_5_1/Program.cs
+++ m_5/m_5_1/Program.cs
@@ -2,98 +2,149 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Net;
-using System.Collections;
-using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace m_5_1
+using System.Collections;
+
+using System.IO;
+
+using System.Net;
+
+using System.Text.RegularExpressions;
+
+namespace Crawler
 {
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String startUrl = "http://www.cnblogs.com/";
+            if (args.Length >= 1) startUrl = args[0];
+            Crawler myCrawler = new Crawler(startUrl);
+            new Thread(myCrawler.Crawl).Start();
+        }
 
 
         public class Crawler
         {
-            private Hashtable urls = new Hashtable();
-            private int count = 0;
+            private Hashtable urls;
+            private Queue<String> queue;
+            private int count;
+            private String originalSite;  //原始网址
 
-            static void Main(string[]args)
-        {
-            Crawler myCrawler = new Crawler();
-            string startUrl = "http://www.cnblogs.com";
-            if (args.Length >= 1) startUrl = args[0];
 
-            myCrawler.urls.Add(startUrl, false);
-
-            new Thread(myCrawler.Crawl).Start();
-            
-        }
-
-        private void Crawl()
-        {
-            Console.WriteLine("开始爬行了....");
-            while(true)
+            public Crawler(String startUrl)
             {
-                string current = null;
-                foreach(string url in urls.Keys)
+                urls = new Hashtable();
+                urls.Add(startUrl, false);
+                queue = new Queue<String>();
+                queue.Enqueue(startUrl);
+                count = 0;
+
+                //求原始网址
+                int startIndex = startUrl.IndexOf(":") + 3, endIndex = startUrl.IndexOf("/", startIndex);
+                originalSite = startUrl.Substring(startIndex, endIndex - startIndex);
+            }
+
+            public void Crawl()
+            {
+                Console.WriteLine("开始爬行了.... ");
+                while (true)
                 {
-                    if ((bool)urls[url]) continue;
-                    current = url;
+                    string current;
+                    do
+                    {
+                        current = null;
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                        current = queue.Dequeue();
+                    } while ((bool)urls[current]);  //防止爬取相同网页
+                    if (current == null || count > 100)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("爬行" + current + "页面!");
+                    string html = DownLoad(current); // 下载
+                    urls[current] = true;
+                    count++;
+                    Parse(current, html);  //解析,并加入新的链接
+                    Console.WriteLine("爬行结束");
                 }
-                if (current == null || count > 10) break;
-
-                Console.WriteLine("爬行" + current + "页面!");
-
-                string html = DownLoad(current);
-
-                urls[current] = true;
-                count++;
-
-                Parse(html);
             }
-            Console.WriteLine("爬行结束");
-        }
 
-        public string DownLoad(string url)
-        {
-            try
+            public string DownLoad(string url)
             {
-                WebClient webClient = new WebClient();
-                webClient.Encoding = Encoding.UTF8;
-                string html = webClient.DownloadString(url);
-
-                string fileName = count.ToString();
-                File.WriteAllText(fileName, html, Encoding.UTF8);
-                return html;
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.Encoding = Encoding.UTF8;
+                    string html = webClient.DownloadString(url);
+                    string fileName = count.ToString();
+                    File.WriteAllText(fileName + ".html", html, Encoding.UTF8);
+                    return html;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return "";
+                }
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return "";
-            }
-        }
 
-        public void Parse(string html)
-        {
-            //string strRef = @"(href|HREF)[]*=[]*[""'][^""#>]+[""']";
-            string strRef = @"(href|HREF)\s*=\s*[""'](?<url>[^""'#>]+)[""']";
-            MatchCollection matches = new Regex(strRef).Matches(html);
-            foreach(Match match in matches)
+            private void Parse(string current, string html)
             {
-                strRef = match.Value.Substring(match.Value.IndexOf('=')+1).Trim('"','\"','#',' ','>');
+                string strRef = @"(href|HREF)[ ]*=[ ]*[""'][^""'#>]+[""']";
 
+                //求当前网页的网页前缀，用于将相对地址转换为绝对地址
+                int endIndex = current.IndexOf("/", current.IndexOf(":") + 3);
+                string prefix = current.Substring(0, endIndex);
+                string protocol = current.Substring(0, current.IndexOf(":"));
+
+                string htmlPattern = @"(.html|.htm)";
+                MatchCollection matches = new Regex(strRef).Matches(html);
+                foreach (Match match in matches)
+                {
+                    strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
                     if (strRef.Length == 0) continue;
-
-                if (urls[strRef] == null) urls[strRef] = false;
+                    //将相对地址转换为绝对地址
+                    if (!Regex.IsMatch(strRef, @"://"))
+                    {
+                        if (Regex.IsMatch(strRef, @"^[a-zA-Z0-9]"))
+                        {
+                            strRef = new Regex(@"[^/]+$").Replace(current, strRef);
+                        }
+                        else if (strRef.StartsWith("//"))
+                        {
+                            strRef = protocol + ":" + strRef;
+                        }
+                        else if (strRef.StartsWith("/"))
+                        {
+                            strRef = prefix + strRef;
+                        }
+                        else if (strRef.StartsWith("./"))
+                        {
+                            strRef = new Regex(@"/[^/]/$").Replace(current, strRef.Substring(2, strRef.Length - 2));
+                        }
+                        else if (strRef.StartsWith("../"))
+                        {
+                            while (strRef.StartsWith("../"))
+                            {
+                                strRef = new Regex(@"[^/]+/[^/]+$").Replace(current, strRef);
+                            }
+                        }
+                    }
+                    if (!Regex.IsMatch(strRef, originalSite)) continue;  //只爬取原始网站上的信息
+                    if (!Regex.IsMatch(strRef, htmlPattern)) continue;  //只爬取html文本
+                    if (urls[strRef] == null)
+                    {
+                        urls.Add(strRef, false);
+                        queue.Enqueue(strRef);
+                    }
+                }
             }
         }
 
-        }
-
-    
-
-
-
+    }
 }
---
+++ test/test/Model1.cs
@@ -0,0 +1,98 @@
+﻿namespace test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Model1 : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“Model1”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“test.Model1”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“Model1”
+        //连接字符串。
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Url { get; set; }
+            public int Rating { get; set; }
+            public List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public string Comment { get; set; }
+
+            public int BlogId { get; set; }
+            public Blog Blog { get; set; }
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public BloggingContext() : base("BlogDataBase")
+            {
+                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<BloggingContext>());
+            }
+
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        }
+
+        public void AddBlog()
+        {
+            using (var db = new BloggingContext())
+            {
+                var blog = new Blog { Url = "http://sample.com", Rating = 3 };
+                blog.Posts = new List<Post>()
+            {
+                new Post(){Title="Test1",Content = "Hello"},
+                new Post(){Title="Test2",Content="Hello"}
+            };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+
+        }
+
+        public void AddPost()
+        {
+            using(var db = new BloggingContext())
+            {
+                var post = new Post { Title = "Test3", Content = "Hello", BlogId = 1 };
+                db.Entry(post).State = EntityState.Added;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeletePost()
+        {
+
+        }
+
+
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+    
+
+
+}
